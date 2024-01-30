c1738b7be73d4f44d778b9a534a356387bf3b549
Marius Schröder 🚀 <schroeder@medialesson.de>
2024-01-30 12:17:09 +0100

    Adjusted imports

--- apps/nx-with-transloco/src/main.ts
+++ apps/nx-with-transloco/src/main.ts
@@ -1,9 +1,9 @@
-import { bootstrapApplication } from "@angular/platform-browser";
-import { AppComponent } from "./app/app.component";
-import { importProvidersFrom, isDevMode } from "@angular/core";
-import { HttpClientModule } from "@angular/common/http";
-import { provideTransloco } from "@ngneat/transloco";
-import { TranslocoHttpLoader } from "./transloco-http.loader";
+import { bootstrapApplication } from '@angular/platform-browser';
+import { AppComponent } from './app/app.component';
+import { importProvidersFrom, isDevMode } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { provideTransloco } from '@ngneat/transloco';
+import { TranslocoHttpLoader } from './transloco-http.loader';
 
 bootstrapApplication(AppComponent, {
   providers: [
